b6ea35e4d7a5aa3247263f6424e75d92e6776246
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-08-08 20:15:06 -0400

    Nutrition Manager Implemented & Made Successful Calls
    Nutrition manager implemented and made successful API calls and retrieved the top item that is returned

--- Barcode Scanner/NutritionScanner.swift
+++ Barcode Scanner/NutritionScanner.swift
@@ -11,6 +11,64 @@ import Vision
 import SwiftUI
 import VisionKit
 
+class NutrionManager : NSObject{
+    
+    static let nutritionManager = NutrionManager()
+    
+    struct API{
+        static let apiURL = "https://api.nal.usda.gov/fdc/v1"
+    }
+    
+    enum APIResponseError : Error{
+        case FailedToGetData
+        case AuthenticationError
+        case NoFoodHits
+    }
+    
+    enum HTTPResp : String{
+        case GET
+        case POST
+        case PUT
+    }
+    
+    override init(){
+        
+    }
+    
+    func searchBarcodeID(with barcodeNumber: String,completion: @escaping (Result<Food,Error>)->Void){
+        guard let url = URL(string: "\(API.apiURL)/foods/search?api_key=Fb4ewYXWfglKqzNXndfKc9G16rAyqbi71h0VcSiI&query=\(barcodeNumber)") else{
+            return
+        }
+        
+        let apiTask = URLSession.shared.dataTask(with: url) { data, resp, err in
+            guard let data = data , err == nil else{
+                completion(.failure(APIResponseError.FailedToGetData))
+                return
+            }
+            do{
+                var data = try JSONDecoder().decode(FoodRequest.self, from: data)
+                
+                if data.totalHits == 0{
+                    completion(.failure(APIResponseError.NoFoodHits))
+                }
+                
+                completion(.success(data.foods[0]))
+            }catch{
+                print("Barcode Search Info: \(String(describing: err?.localizedDescription))")
+                completion(.failure(error))
+            }
+        }
+        
+        apiTask.resume()
+        
+    }
+    
+    func searchFoodList(with foodName: String){
+        
+    }
+    
+}
+
 struct NutritionScannerScreen : View {
     var body: some View {
         CameraScanner()
@@ -121,6 +179,18 @@ struct BarcodeScannerViewController : UIViewControllerRepresentable{
             case .barcode(let barcode):
                 self.parent.barcodeText = barcode.payloadStringValue ?? ""
                 print("Barcode Text Scanned : \(parent.barcodeText)")
+                _Concurrency.Task{
+                    NutritionManager.nutritionManager.searchBarcodeID(with: String(String(parent.barcodeText).dropFirst())) { res in
+                        switch res{
+                        case .success(let food):
+                            print(food)
+                            break
+                        case .failure(let error):
+                            print(error)
+                            break
+                        }
+                    }
+                }
                 break
             case .text(let text):
                 self.parent.barcodeText = text.transcript
--- Managers/NutritionManager.swift
+++ Managers/NutritionManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class NutrionManager{
+class NutritionManager : NSObject{
     
     static let nutritionManager = NutrionManager()
     
@@ -18,20 +18,44 @@ class NutrionManager{
     enum APIResponseError : Error{
         case FailedToGetData
         case AuthenticationError
+        case NoFoodHits
     }
     
-    enum HTTPRequest : String{
+    enum HTTPResp : String{
         case GET
         case POST
         case PUT
     }
     
-    init(){
+    override init(){
         
     }
     
-    func searchBarcodeID(with barcodeId: String){
+    func searchBarcodeID(with barcodeNumber: String,completion: @escaping (Result<Food,Error>)->Void){
+        guard let url = URL(string: "\(API.apiURL)/foods/search?api_key=Fb4ewYXWfglKqzNXndfKc9G16rAyqbi71h0VcSiI&query=\(barcodeNumber)") else{
+            return
+        }
         
+        let apiTask = URLSession.shared.dataTask(with: url) { data, resp, err in
+            guard let data = data , err == nil else{
+                completion(.failure(APIResponseError.FailedToGetData))
+                return
+            }
+            do{
+                var data = try JSONDecoder().decode(FoodRequest.self, from: data)
+                
+                if data.totalHits == 0{
+                    completion(.failure(APIResponseError.NoFoodHits))
+                }
+                
+                completion(.success(data.foods[0]))
+            }catch{
+                print("Barcode Search Info: \(String(describing: err?.localizedDescription))")
+                completion(.failure(error))
+            }
+        }
+        
+        apiTask.resume()
         
     }
     
