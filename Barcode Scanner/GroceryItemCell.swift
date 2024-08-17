7a17eb2a49fcd025482f95dc3ed570b7abe82bd4
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-08-16 23:18:13 -0400

    Refactored Models To Independent Files  For Easier Debugging
    Refactored models to the independent files to allow for better development, tracking, and debugging

---
+++ Barcode Scanner/GroceryItemCell.swift
@@ -0,0 +1,38 @@
+//
+//  GroceryItemCell.swift
+//  TimeCraft
+//
+//  Created by Akhilesh Warty on 2024-08-15.
+//
+
+import Foundation
+import SwiftUI
+
+struct GroceryItemCell : View {
+    
+    let scannedFood : GroceryItem
+    
+    private let navigationController : UINavigationController
+    
+    init(scannedFood: GroceryItem, navigationController: UINavigationController) {
+        self.scannedFood = scannedFood
+        self.navigationController = navigationController
+    }
+    
+    var body: some View {
+        HStack{
+            VStack{
+                Text("\(scannedFood.title)").truncationMode(.tail)
+                Text("\(scannedFood.brand)").font(.caption)
+            }
+            Button(){
+                navigationController.pushViewController(GroceryItemViewController(foodItem: scannedFood), animated: true)
+            }label: {
+                Label(
+                    title: { Text("") },
+                    icon: { Image(systemName: "chevron.right").foregroundColor(Color(UIColor.label)) }
+                )
+            }.frame(maxWidth: .infinity,alignment: .trailing).controlSize(.small).frame(maxWidth: .infinity,alignment: .trailing)
+        }
+    }
+}
